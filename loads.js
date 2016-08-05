283ca979713e21e555e61477662e0f71313e6a49
zhump <695004175@qq.com>
2016-08-06 00:25:15 +0800

    异步加载资源文件
    异步加载js、css文件

---
+++ loads.js
@@ -0,0 +1,57 @@
+window.loads = function(_urls,callMyFun){
+	var _ld = {
+		js:function(_url,callback){
+			var _scipt = document.createElement("script");
+			_scipt.setAttribute("type", "text/javascript");
+			_scipt.setAttribute("src", _url);
+			document.getElementsByTagName("head")[0].appendChild(_scipt);
+			if (navigator.userAgent.indexOf("IE") >= 0) {
+				_scipt.onreadystatechange = function() {
+					if (_scipt && (_scipt.readyState == "loaded" || _scipt.readyState == "complete")) {
+						_scipt.onreadystatechange = null;
+						callback ? callback() : '';
+					}
+				};
+			} else {
+				_scipt.onload = function() {
+					_scipt.onload = null;
+					callback ? callback() : '';
+				};
+			}
+		},
+		css:function(_url,callback){
+			var _head = document.getElementsByTagName('head')[0],
+				_link = document.createElement('link');
+				_link.setAttribute("type", "text/css");
+				_link.setAttribute("rel", "stylesheet");
+				_link.setAttribute("href", _url);
+				_head.appendChild(_link);
+				
+				_link.onload = function() {
+					_head.onload = null;
+					callback ? callback() : '';
+				};
+		}
+	};
+	var index = 0;
+	if(_urls && _urls.length > 0){
+		load(_urls[index]);
+	}
+	function load(_url){
+		_ld[_url.indexOf('css!') == 0 ? "css":"js"]((function(_url){
+			return _url.replace('css!','').replace('js!','');
+		})(_url),function(){
+			index++;
+			if(index == _urls.length){
+				callMyFun();
+			}else{
+				load(_urls[index]);
+			}
+		});
+	}
+}
+
+//loads(['css!http://192.168.1.103:8020/css/lib.css','js!http://192.168.1.103:8020/js/lib/zm.js'],function(){
+	//console.log('加载完成')
+//})
+
