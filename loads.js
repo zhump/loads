62a07728d591be0ffbee49a2289f730174f363ce
zhump <695004175@qq.com>
2016-08-06 13:55:54 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -51,8 +51,3 @@ window.loads = window.loads || function(_urls,callMyFun,async){
 		!async || (++index == _urls.length ? callMyFun():load(_urls[index]));
 	}
 }
-
-//loads(['css!http://172.31.0.115:8020/css/index.css','js!http://172.31.0.115:8020/js/index.js'],function(){
-    //console.log('加载完成')
-//},'async'); // 设置最后一个参数为'async',加载资源为异步加载，缺省值为同步加载
-
